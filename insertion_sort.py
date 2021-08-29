e1eaa30d0ec59ef9c3536af4debbc75b30cc3949
Myke Amorim <mykeleony147@gmail.com>
2021-08-28 23:54:07 -0300

    Create insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,3 +1,9 @@
+'''
+Programa que utiliza o algoritmo Insertion Sort, retirado do livro "Introduction to Algorithms" para ordenar uma sequência aleatória de números em Python.
+Myke Leony dos Santos Amorim. 24 de agosto de 2021.
+Introdução à Análise de Algoritmos - Sistemas de Informação: USP.
+'''
+
 lista_num = input('Insira os números (separados por espaços): ').split(' ')
 
 for numero in numeros:   # Iniciar o loop no a partir do segundo elemento.
