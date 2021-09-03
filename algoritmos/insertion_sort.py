0394229b4a23e3503692a8a15a55dfe04ba070a8
Myke Amorim <mykeleony147@gmail.com>
2021-09-02 23:57:07 -0300

    Update insertion_sort.py

--- algoritmos/insertion_sort.py
+++ algoritmos/insertion_sort.py
@@ -1,6 +1,6 @@
 '''
 Programa que utiliza o algoritmo Insertion Sort, retirado do livro "Introduction to Algorithms" para ordenar uma sequência aleatória de números em Python.
-Myke Leony dos Santos Amorim. 24 de agosto de 2021.
+Myke Leony dos Santos Amorim. 25 de agosto de 2021.
 Introdução à Análise de Algoritmos - Sistemas de Informação: USP.
 '''
 
