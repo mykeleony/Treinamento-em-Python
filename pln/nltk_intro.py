e7a2681de288474c0b0ceef6c89c2fe967588c45
Myke Amorim <mykeleony147@gmail.com>
2021-10-24 18:40:33 -0300

    Create nltk_intro.py

---
+++ pln/nltk_intro.py
@@ -0,0 +1,15 @@
+'''
+Programa que realiza manipulações elementares no Natural Language ToolKit.
+Myke Leony dos Santos Amorim. 15 de setembro de 2021.
+Curso: Python para Processamento de Linguagem Natural - Instituto de Ciências Matemáticas e de Computação (USP).
+'''
+
+import nltk
+
+nltk.download() # Para acessar algum corpus, é necessário baixá-lo previamente.
+
+print(nltk.corpus.mac_morpho.words()) # Acessando o corpus de português brasileiro Mac-Morpho:
+print('Este corpus possui '+str(len(nltk.corpus.mac_morpho.words()))+' palavras.')
+
+print(nltk.corpus.mac_morpho.sents()[0]) # Acessando a primeira sentença do Mac-Morpho.
+print(nltk.corpus.mac_morpho.tagged_words()) # Acessando as palavras com suas respectivas classes gramaticais em tuplas.
