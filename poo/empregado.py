910d2028406d44187e5d128f971ddbeb14029974
Myke Amorim <mykeleony147@gmail.com>
2021-12-22 00:32:20 -0300

    Criação da superclasse "Empregado".

---
+++ poo/empregado.py
@@ -0,0 +1,10 @@
+class Empregado:
+    def __init__(self, nome, cpf, rg):
+        self.nome = nome
+        self.cpf = cpf
+        self.rg = rg
+
+    def pagamento (self):
+        print('O pagamento é um momento de grande felicidade para o funcionário!')
+
+        return 0
