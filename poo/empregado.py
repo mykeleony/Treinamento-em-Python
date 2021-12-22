d5e664399cc6c3516e37f35bbcf3d6ac214a53a1
Myke Amorim <mykeleony147@gmail.com>
2021-12-22 00:32:47 -0300

    Implementação da subclasse "EmpregadoHorista".

--- poo/empregado.py
+++ poo/empregado.py
@@ -8,3 +8,12 @@ class Empregado:
         print('O pagamento é um momento de grande felicidade para o funcionário!')
 
         return 0
+
+class EmpregadoHorista (Empregado):
+    def __init__(self, nome, cpf, rg, horas_trab, salario_hora):
+        Empregado.__init__(self, nome, cpf, rg)
+        self.horas_trab = horas_trab
+        self.salario_hora = salario_hora
+
+    def pagamento (self):
+        return self.horas_trab * self.salario_hora
